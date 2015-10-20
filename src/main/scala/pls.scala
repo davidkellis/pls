1e24c8eec139266d603cd8b37964bb56b5f1a850
David Ellis <davidkellis@gmail.com>
2015-10-19 23:33:43 -0500

    close to having a working test

--- src/main/scala/pls.scala
+++ src/main/scala/pls.scala
@@ -1,11 +1,11 @@
 package com.github.davidkellis.pls
 
-import scala.annotation.tailrec
-
 import breeze.linalg._
 import breeze.linalg.eigSym.EigSym
 import breeze.numerics._
 import breeze.stats._
+
+import scala.annotation.tailrec
 
 object Utils {
   // perform column-wise sum; given M (RxC), yields a (1xC) matrix of per-column sums
@@ -46,94 +46,125 @@ object Standardize {
   def centerAndScaleColumns(M: DenseMatrix[Double]): DenseMatrix[Double] = scaleColumns(meanCenterColumns(M))
 }
 
-object DayalMcGregor {
-  // B - PLS regression coefficients matrix (K × M)
-  // W - PLS weights matrix for X (K × A)
-  // P - PLS loadings matrix for X (K × A)
-  // Q - PLS loadings matrix for Y (M × A)
-  // R - PLS weights matrix to compute scores T directly from original X (K × A)
-  case class BWPQR(B: DenseMatrix[Double], W: DenseMatrix[Double], P: DenseMatrix[Double], Q: DenseMatrix[Double], R: DenseMatrix[Double])
+//abstract class PlsModel[ModelT](var model: ModelT) {
+//  // X - predictor variables matrix (N × K)
+//  // Y - response variables matrix (N × M)
+//  // A - is the number of latent variables (a.k.a. components) to use
+//  def train(X: DenseMatrix[Double], Y: DenseMatrix[Double], A: Int): ModelT
+//  def predict(X: DenseMatrix[Double]): DenseMatrix[Double]
+//}
 
+trait PlsModel[ModelT] {
   // X - predictor variables matrix (N × K)
   // Y - response variables matrix (N × M)
-  // B_PLS - PLS regression coefficients matrix (K × M)
+  // A - is the number of latent variables (a.k.a. components) to use
+  def train(X: DenseMatrix[Double], Y: DenseMatrix[Double], A: Int): ModelT
+  def predict(model: ModelT, X: DenseMatrix[Double]): DenseMatrix[Double]
+}
+
+object DayalMcGregor {
+  // Beta - PLS regression coefficients matrix (K × M)
   // W - PLS weights matrix for X (K × A)
   // P - PLS loadings matrix for X (K × A)
   // Q - PLS loadings matrix for Y (M × A)
   // R - PLS weights matrix to compute scores T directly from original X (K × A)
-  // T - PLS scores matrix of X (N × A)
-  // w_a - a column vector of W
-  // p_a - a column vector of P
-  // q_a - a column vector of Q
-  // r_a - a column vector of R
-  // t_a - a column vector of T
-  // K - number of X-variables
-  // M - number of Y-variables
-  // N - number of objects
-  // A - number of components in PLS model
-  // a - integer counter for latent variable dimension.
+  case class Model(Beta: DenseMatrix[Double], W: DenseMatrix[Double], P: DenseMatrix[Double], Q: DenseMatrix[Double], R: DenseMatrix[Double])
 
-  /**
-   * Implements "Modified kernel algorithm #2" as described in Dayal and MacGregor's "Improved PLS Algorithms" paper,
-   * published in Journal of Chemometrics, Volume 11, Issue 1, pages 73–85, January 1997.
-   *
-   * X - predictor variables matrix (N x K); K - number of X-variables
-   * Y - response variables matrix (N x M); M - number of Y-variables
-   * A - number of components in PLS model
-   */
-  def pls2(X: DenseMatrix[Double], Y: DenseMatrix[Double], A: Int): BWPQR = {
-    val K = X.cols  // number of columns in X - number of predictor variables
-    val M = Y.cols  // number of columns in Y - number of response variables
-    val N = X.rows  // number of rows in X === number of rows in Y - number of observations of predictor and response variables
-    var W: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, A)      // (K × A)
-    var P: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, A)      // (K × A)
-    var Q: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, A)      // (M × A)
-    var R: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, A)      // (K × A)
-    var YtXXtY: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, M) // (M x M) matrix
-    var w_a: DenseVector[Double] = DenseVector.zeros[Double](K)       // (K x 1) - column vector of W
-    var p_a: DenseVector[Double] = DenseVector.zeros[Double](M)       // (K x 1) - column vector of P
-    var q_a: DenseVector[Double] = DenseVector.zeros[Double](M)       // (M x 1) - column vector of Q
-    var r_a: DenseVector[Double] = DenseVector.zeros[Double](K)       // (K x 1) - column vector of R
-    var tt: Double = 0.0
-    var indexOfLargestEigenvalue: Int = 0
-    var i: Int = 0
+  object Algorithm2 extends PlsModel[Model]{
+    // X - predictor variables matrix (N × K)
+    // Y - response variables matrix (N × M)
+    // B_PLS - PLS regression coefficients matrix (K × M)
+    // W - PLS weights matrix for X (K × A)
+    // P - PLS loadings matrix for X (K × A)
+    // Q - PLS loadings matrix for Y (M × A)
+    // R - PLS weights matrix to compute scores T directly from original X (K × A)
+    // T - PLS scores matrix of X (N × A)
+    // w_a - a column vector of W
+    // p_a - a column vector of P
+    // q_a - a column vector of Q
+    // r_a - a column vector of R
+    // t_a - a column vector of T
+    // K - number of X-variables
+    // M - number of Y-variables
+    // N - number of objects
+    // A - number of components in PLS model
+    // a - integer counter for latent variable dimension.
 
-    var XY = X.t * Y                                              // compute the covariance matrices; (K x M) matrix
-    var XX = X.t * X                                              // (K x K) matrix
-    for (a <- 1 to A) {                                           // A = number of PLS components to compute
-      i = a - 1                                                   // i is the zero-based index; a is the 1-based index
+    /**
+     * Implements "Modified kernel algorithm #2" as described in Dayal and MacGregor's "Improved PLS Algorithms" paper,
+     * published in Journal of Chemometrics, Volume 11, Issue 1, pages 73–85, January 1997.
+     *
+     * X - predictor variables matrix (N x K); K - number of X-variables
+     * Y - response variables matrix (N x M); M - number of Y-variables
+     * A - number of components in PLS model
+     */
+    def train(X: DenseMatrix[Double], Y: DenseMatrix[Double], A: Int): Model = {
+      val K = X.cols // number of columns in X - number of predictor variables
+      val M = Y.cols // number of columns in Y - number of response variables
+      val N = X.rows // number of rows in X === number of rows in Y - number of observations of predictor and response variables
+      val W: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, A) // (K × A)
+      val P: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, A) // (K × A)
+      val Q: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, A) // (M × A)
+      val R: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, A) // (K × A)
+      var YtXXtY: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, M) // (M x M) matrix
+      var w_a: DenseVector[Double] = DenseVector.zeros[Double](K) // (K x 1) - column vector of W
+      var p_a: DenseVector[Double] = DenseVector.zeros[Double](M) // (K x 1) - column vector of P
+      var q_a: DenseVector[Double] = DenseVector.zeros[Double](M) // (M x 1) - column vector of Q
+      var r_a: DenseVector[Double] = DenseVector.zeros[Double](K) // (K x 1) - column vector of R
+      var tt: Double = 0.0
+      var indexOfLargestEigenvalue: Int = 0
+      var i: Int = 0
 
-      if (M == 1) {                                               // if there is a single response variable, compute the X-weights as:
-        w_a = XY.toDenseVector                                    // in this case, XY (K x M) is a column vector (K x 1) since M == 1
-      } else {                                                    // otherwise there are multiple response variables, so compute the X-weights as:
-        // The source code of pls/R/kernelpls.fit.R in R's "pls" package (see https://cran.r-project.org/web/packages/pls/index.html
-        // or http://mevik.net/work/software/pls.html) states that YtXXtY is a symmetric matrix.
-        // The fact that YtXXtY is **wonderful** because all eigenvalues of a real-valued symmetric matrix are real values.
-        // Otherwise, we'd have to potentially handle Complex-valued eigenvalues.
-        YtXXtY = XY.t * XY                                        // XY.t * XY is an (M x M) matrix
-        val EigSym(eigenvalues, eigenvectors) = eigSym(YtXXtY)    // eigenvalues is a DenseVector[Double] and eigenvectors is a DenseMatrix[Double]
-        indexOfLargestEigenvalue = argmax(eigenvalues)            // find index of largest eigenvalue
-        q_a = eigenvectors(::, indexOfLargestEigenvalue)          // find the eigenvector corresponding to the largest eigenvalue; eigenvector is (M x 1)
-        w_a = XY * q_a                                            // compute X-weights; w_a is (K x 1)
+      println("***")
+      println(X.t)
+      println(Y)
+
+      var XY: DenseMatrix[Double] = X.t * Y // compute the covariance matrices; (K x M) matrix
+      val XX: DenseMatrix[Double] = X.t * X // (K x K) matrix
+      for (a <- 1 to A) {
+        // A = number of PLS components to compute
+        i = a - 1 // i is the zero-based index; a is the 1-based index
+
+        if (M == 1) {
+          // if there is a single response variable, compute the X-weights as:
+          w_a = XY.toDenseVector // in this case, XY (K x M) is a column vector (K x 1) since M == 1
+        } else {
+          // otherwise there are multiple response variables, so compute the X-weights as:
+          // The source code of pls/R/kernelpls.fit.R in R's "pls" package (see https://cran.r-project.org/web/packages/pls/index.html
+          // or http://mevik.net/work/software/pls.html) states that YtXXtY is a symmetric matrix.
+          // The fact that YtXXtY is **wonderful** because all eigenvalues of a real-valued symmetric matrix are real values.
+          // Otherwise, we'd have to potentially handle Complex-valued eigenvalues.
+          YtXXtY = XY.t * XY // XY.t * XY is an (M x M) matrix
+          val EigSym(eigenvalues, eigenvectors) = eigSym(YtXXtY) // eigenvalues is a DenseVector[Double] and eigenvectors is a DenseMatrix[Double]
+          indexOfLargestEigenvalue = argmax(eigenvalues) // find index of largest eigenvalue
+          q_a = eigenvectors(::, indexOfLargestEigenvalue) // find the eigenvector corresponding to the largest eigenvalue; eigenvector is (M x 1)
+          w_a = XY * q_a // compute X-weights; w_a is (K x 1)
+        }
+        w_a = w_a / sqrt(w_a.t * w_a) // normalize w_a to unity
+        r_a = w_a // loop to compute r_a
+        for (j <- 1 to (a - 1)) {
+          r_a = r_a - (P(::, j).t * w_a) * R(::, j)
+        }
+        tt = r_a.t * XX * r_a // compute t't - (1 x 1)
+        p_a = (r_a.t * XX).t / tt // X-loadings
+        q_a = (r_a.t * XY).t / tt // Y-loadings
+        XY = XY - (p_a * q_a.t) * tt // XtY deflation
+
+        // update loadings and weights
+        W(::, i) := w_a
+        P(::, i) := p_a
+        Q(::, i) := q_a
+        R(::, i) := r_a
       }
-      w_a = w_a / sqrt(w_a.t * w_a)                               // normalize w_a to unity
-      r_a = w_a                                                   // loop to compute r_a
-      for (j <- 1 to (a - 1)) {
-        r_a = r_a - (P(::, j).t * w_a) * R(::, j)
-      }
-      tt = (r_a.t * XX * r_a)                                     // compute t't - (1 x 1)
-      p_a = (r_a.t * XX).t / tt                                   // X-loadings
-      q_a = (r_a.t * XY).t / tt                                   // Y-loadings
-      XY = XY - (p_a * q_a.t) * tt                                // XtY deflation
+      val beta = R * Q.t // compute the regression coefficients; (K x M)
 
-      // update loadings and weights
-      W(::, i) := w_a
-      P(::, i) := p_a
-      Q(::, i) := q_a
-      R(::, i) := r_a
+      Model(beta, W, P, Q, R)
     }
-    val beta = R * Q.t                                            // compute the regression coefficients; (K x M)
 
-    BWPQR(beta, W, P, Q, R)
+    // Y = X*B+e
+    def predict(model: Model, X: DenseMatrix[Double]): DenseMatrix[Double] = {
+      X*model.Beta
+    }
   }
+
 }
--- src/test/scala/plsSpec.scala
+++ src/test/scala/plsSpec.scala
@@ -1,10 +1,19 @@
 import org.scalatest._
 
+import breeze.linalg._
+
 import com.github.davidkellis.pls._
 
 class PlsSpec extends FlatSpec {
-  "pls" should "work" in {
-    // assert(something === something else)
-    1
+  "pls" should "predict interest rates as well as simple linear regression (see http://www.cyclismo.org/tutorial/R/linearLeastSquares.html)" in {
+    val X = DenseMatrix((2000.0, 1.0), (2001.0, 1.0), (2002.0, 1.0), (2003.0, 1.0), (2004.0, 1.0))
+    val Y = DenseVector(9.34, 8.50, 7.62, 6.93, 6.60).toDenseMatrix.t             // (5 x 1) matrix
+
+    val model = DayalMcGregor.Algorithm2.train(X, Y, 2)
+
+    val expectedY = DenseVector(-1.367).toDenseMatrix.t
+    val approxY = DayalMcGregor.Algorithm2.predict(model, DenseMatrix((2015.0, 1.0)))
+
+    assert(approxY === expectedY)
   }
 }
