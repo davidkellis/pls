e51b419ca04f5bb85ac373f31d6830645a7baf76
David Ellis <davidkellis@gmail.com>
2015-10-23 12:32:40 -0500

    attempt 1

--- src/main/scala/pls.scala
+++ src/main/scala/pls.scala
@@ -186,13 +186,13 @@ object DayalMcGregor {
       val Q: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, A)        // (M x A)
       val R: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, A)        // (K x A)
       var YtXXtY: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, M)   // (M x M) matrix
-      var w_a: DenseVector[Double] = DenseVector.zeros[Double](K)         // (K x 1) - column vector of W
-      var p_a: DenseVector[Double] = DenseVector.zeros[Double](M)         // (K x 1) - column vector of P
-      var q_a: DenseVector[Double] = DenseVector.zeros[Double](M)         // (M x 1) - column vector of Q
-      var r_a: DenseVector[Double] = DenseVector.zeros[Double](K)         // (K x 1) - column vector of R
+      var w_a: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, 1)      // (K x 1) - column vector of W
+      var p_a: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, 1)      // (K x 1) - column vector of P
+      var q_a: DenseMatrix[Double] = DenseMatrix.zeros[Double](M, 1)      // (M x 1) - column vector of Q
+      var r_a: DenseMatrix[Double] = DenseMatrix.zeros[Double](K, 1)      // (K x 1) - column vector of R
       var tt: Double = 0.0
       var indexOfLargestEigenvalue: Int = 0
-      var i: Int = 0
+      var j: Int = 0
 
 //      println("***")
 //      println(X.t)
@@ -200,40 +200,38 @@ object DayalMcGregor {
 
       var XY: DenseMatrix[Double] = X.t * Y                               // compute the covariance matrices; (K x M) matrix
       val XX: DenseMatrix[Double] = X.t * X                               // (K x K) matrix
-      for (a <- 1 to A) {
+      // A = number of PLS components to compute
+      for (a <- 0 to (A - 1)) {
 
 //        println("XY")
 //        println(XY)
 //        println("XX")
 //        println(XX)
 
-        // A = number of PLS components to compute
-        i = a - 1 // i is the zero-based index; a is the 1-based index
-
         if (M == 1) {
           // if there is a single response variable, compute the X-weights as:
-          w_a = XY.toDenseVector                                          // in this case, XY (K x M) is a column vector (K x 1) since M == 1
+          w_a = XY                                                        // in this case, XY (K x M) is a column vector (K x 1) since M == 1
         } else {
           // otherwise there are multiple response variables, so compute the X-weights as:
           // The source code of pls/R/kernelpls.fit.R in R's "pls" package (see https://cran.r-project.org/web/packages/pls/index.html
           // or http://mevik.net/work/software/pls.html) states that YtXXtY is a symmetric matrix.
           // The fact that YtXXtY is **wonderful** because all eigenvalues of a real-valued symmetric matrix are real values.
           // Otherwise, we'd have to potentially handle Complex-valued eigenvalues.
-          YtXXtY = XY.t * XY                                              // XY.t * XY is an (M x M) matrix
-          val EigSym(eigenvalues, eigenvectors) = eigSym(YtXXtY)          // eigenvalues is a DenseVector[Double] and eigenvectors is a DenseMatrix[Double]
-          indexOfLargestEigenvalue = argmax(eigenvalues)                  // find index of largest eigenvalue
-          q_a = eigenvectors(::, indexOfLargestEigenvalue)                // find the eigenvector corresponding to the largest eigenvalue; eigenvector is (M x 1)
-          w_a = XY * q_a                                                  // compute X-weights; w_a is (K x 1); expression yields a (K x M) * (M x 1) === (K x 1)
+          YtXXtY = XY.t * XY                                                // XY.t * XY is an (M x M) matrix
+          val EigSym(eigenvalues, eigenvectors) = eigSym(YtXXtY)            // eigenvalues is a DenseVector[Double] and eigenvectors is a DenseMatrix[Double]
+          indexOfLargestEigenvalue = argmax(eigenvalues)                    // find index of largest eigenvalue
+          q_a = eigenvectors(::, indexOfLargestEigenvalue).toDenseMatrix.t  // find the eigenvector corresponding to the largest eigenvalue; eigenvector is (M x 1)
+          w_a = XY * q_a                                                    // compute X-weights; w_a is (K x 1); expression yields a (K x M) * (M x 1) === (K x 1)
         }
 //        println("before w_a")
 //        println(w_a)
 
-        w_a = w_a / sqrt(w_a.t * w_a)                                     // normalize w_a to unity - the denominator is a scalar
+        w_a = w_a / sqrt(scalar(w_a.t * w_a))                             // normalize w_a to unity - the denominator is a scalar
         r_a = w_a                                                         // loop to compute r_a
-        for (j <- 1 to (a - 1)) {
-          r_a = r_a - ( (P(::, j).t * w_a) * R(::, j) )                   // (K x 1) - ( (1 x K) * (K x 1) ) * (K x 1) === (K x 1) - scalar * (K x 1)
+        for (j <- 0 to (a - 1)) {
+          r_a = r_a - ( scalar(P(::, j).toDenseMatrix.t * w_a) * R(::, j) ).toDenseMatrix.t             // (K x 1) - ( (1 x K) * (K x 1) ) * (K x 1) === (K x 1) - scalar * (K x 1)
         }
-        tt = r_a.t * XX * r_a                                             // compute t't - (1 x 1) that is auto-converted to a scalar
+        tt = scalar(r_a.t * XX * r_a)                                     // compute t't - (1 x 1) that is auto-converted to a scalar
         p_a = (r_a.t * XX).t / tt                                         // X-loadings - ((K x 1)' * (K x K))' / tt === (K x 1) / tt
         q_a = (r_a.t * XY).t / tt                                         // Y-loadings - ((K x 1)' * (K x M))' / tt === (M x 1) / tt
         XY = XY - ((p_a * q_a.t) * tt)                                    // XtY deflation
@@ -248,10 +246,10 @@ object DayalMcGregor {
 //        println(r_a)
 
         // update loadings and weights
-        W(::, i) := w_a
-        P(::, i) := p_a
-        Q(::, i) := q_a
-        R(::, i) := r_a
+        W(::, a) := w_a.toDenseVector
+        P(::, a) := p_a.toDenseVector
+        Q(::, a) := q_a.toDenseVector
+        R(::, a) := r_a.toDenseVector
       }
       val beta = R * Q.t // compute the regression coefficients; (K x M)
 
